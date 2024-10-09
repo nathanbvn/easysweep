295d2c94ff21d486294dba192d31e6288dd6ed1d
nathanbvn <94560509+nathanbvn@users.noreply.github.com>
2024-10-09 11:03:32 +0200

    Update easysweep.py

--- easysweep.py
+++ easysweep.py
@@ -1,43 +1,35 @@
 import ipaddress
-import sys 
+import sys
 import subprocess
-import threading
+import concurrent.futures  
 
 
 def pingIp(addr):
     result = subprocess.run(
-        'ping -c 1 '+addr, 
-        shell=True, 
-        executable="/bin/bash", 
-        stdout=subprocess.DEVNULL,  
-        stderr=subprocess.DEVNULL   
+        'ping -c 1 ' + addr,
+        shell=True,
+        executable="/bin/bash",
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
     )
-    if "returncode=0" in str(result): 
-        ipCo = str(result).split("-c 1 ")[1].split("'")[0]
-        print(ipCo)
+    if result.returncode == 0:  
+        print(addr)
+
 
 args = sys.argv
 
-if len(args) < 2 :
+if len(args) < 2:
     print("\nEasySweep requires an argument !! \n")
     print("Correct format : python3 easysweep.py [network_ip]/[mask] \n")
-else :
+else:
     ip = args[1]
     n1 = ipaddress.ip_network(ip)
-    
+
     print("------------- EasySweep ------------------")
     print("    SCANNING NETWORK !")
     print("CONNECTED IP's : \n")
 
-    threads = []
     
-    for addr in list(n1):
-        thread = threading.Thread(target=pingIp, args=(str(addr),))
-        threads.append(thread)
-        thread.start()
-
-    
-    for thread in threads:
-        thread.join()
-
-
+    with concurrent.futures.ThreadPoolExecutor(max_workers=255) as executor:
+        
+        executor.map(pingIp, (str(addr) for addr in n1))
